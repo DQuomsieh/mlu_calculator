b684951c1b6873d2eb59b54e29a840022ef7657d
Daniel Quomsieh <dquomsieh@gmail.com>
2019-12-15 23:45:51 +0200

    added mlu calculator

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, TextInput} from 'react-native';
 import { Audio } from 'expo-av'
-import {Form, Textarea, Item, Content, Input, Right, Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Button } from 'native-base';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import {Form, Textarea, Item, Textnput, Content, Input, Right, Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Button, Label, Title } from 'native-base';
 import LastPage from './components/LastPage';
 
 const cards = [
@@ -22,19 +24,122 @@ const cards = [
   },
 ];
 
-export default class MainPage extends React.Component{
+export default class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    // this.recording = null;
+    // this.sound = null;
+    // this.isSeeking = false;
+    this.state = {
+      totalMLU: 0,
+      inputOne: '',
+      inputTwo: '',
+      inputThree: '',
+      fontLoaded: false,
+    };
+  }
 
   async componentDidMount() {
     await Font.loadAsync({
-      "Roboto Regular": require('./assets/fonts/Roboto-Regular.ttf')
+      'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf')
     })
-    this.setState({ fontLoaded: true })
+    this.setState({ 
+      fontLoaded: true,
+    });
+  }
+
+  // _askForPermissions = async () => {
+  //   const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+  //   this.setState({
+  //     haveRecordingPermissions: response.status === 'granted',
+  //   });
+  // };
+
+  handleMLU = () => {
+    var words1 = this.state.inputOne.match(/\S+/g);
+    var words2 = this.state.inputTwo.match(/\S+/g);
+    var words3 = this.state.inputThree.match(/\S+/g);
+    var i = 0;
+    var oneMLU = 0;
+    var twoMLU = 0;
+    var threeMLU = 0;
+    alert(words3);
+    if(words1 !== null){
+      while(i != words1.length){
+        if(words1[i].length > 0){
+          if(words1[i].charAt(words1[i].length - 1) === 's'){
+            if(words1[i] === 'this' || words1[i] === 'is' || words1[i] === 'his'){
+              oneMLU += 1;
+            } else {
+              oneMLU += 2;
+            }
+          } else if (words1[i].slice(-3) === 'ing'){
+            oneMLU += 2;
+          } else {
+            oneMLU += 1;
+          }
+        }
+        i++;
+      }
+    }
+    i = 0;
+    if(words2 !== null){
+      while(i != words2.length){
+        if(words2[i].length > 0){
+          if(words2[i].charAt(words2[i].length - 1) === 's'){
+            if(words2[i] === 'this' || words2[i] === 'is' || words2[i] === 'his'){
+              twoMLU += 1;
+            } else {
+              twoMLU += 2;
+            }
+          } else if (words2[i].slice(-3) === 'ing'){
+            twoMLU += 2;
+          } else {
+            twoMLU += 1;
+          }
+        }
+        i++;
+      }
+    }
+    i = 0;
+    if(words3 !== null){
+      while(i != words3.length){
+        if(words3[i].length > 0){
+          if(words3[i].charAt(words3[i].length - 1) === 's'){
+            if(words3[i] === 'this' || words3[i] === 'is' || words3[i] === 'his'){
+              threeMLU += 1;
+              alert(threeMLU);
+            } else {
+              threeMLU += 2;
+              alert(threeMLU);
+            }
+          } else if (words3[i].slice(-3) === 'ing'){
+            threeMLU += 1;
+            alert(threeMLU);
+          } else {
+            threeMLU += 1;
+            alert(threeMLU);
+          }
+        }
+        i++;
+      }
+    }
+    var total = (oneMLU + twoMLU + threeMLU) / 3;
+    
+    this.setState({totalMLU: (Math.round(total * 10) / 10).toFixed(1)});
   }
 
   render() {
     return (
-      <Container>
-      <Header />
+      <Container style={{paddingTop: 24}}>
+      <Header>
+        <Left />
+        <Body>
+          <Title>MLA Calculator</Title>
+        </Body>
+        <Right />
+      </Header>
       <View>
         <DeckSwiper
         dataSource={cards}
@@ -44,21 +149,52 @@ export default class MainPage extends React.Component{
             <View style={{flexDirection: 'column', justifyContent: 'space-evenly', padding: 10}}>
               <Item style={{paddingTop: 20}}>
                 <Icon active name="microphone" />
-                <Input placeholder="Enter sentence for picture #1" />
+                <TextInput 
+                  style={{height: 50, fontSize: 18}}
+                  placeholder="Enter sentence for picture #1"
+                  placeholderTextColor = "#9a73ef"
+                  onChangeText={(inputOne) => this.setState({inputOne})}
+                />
+                {/* <Input placeholder="Enter sentence for picture #1" /> */}
               </Item>
               <Item style={{paddingTop: 20}}>
                 <Icon active name="microphone" />
-                <Input placeholder="Enter sentence for picture #2" />
+                <TextInput 
+                  style={{height: 50, fontSize: 18}}
+                  placeholder="Enter sentence for picture #2"
+                  placeholderTextColor = "#9a73ef"
+                  onChangeText={(inputTwo) => this.setState({inputTwo})}
+                  
+                />
+                {/* <Input placeholder="Enter sentence for picture #2" /> */}
               </Item>
               <Item style={{paddingTop: 20}}>
                 <Icon active name="microphone" />
-                <Input placeholder="Enter sentence for picture #3" />
+                <TextInput 
+                  style={{height: 50, fontSize: 18}}
+                  placeholder="Enter sentence for picture #3"
+                  placeholderTextColor = "#9a73ef"
+                  onChangeText={(inputThree) => this.setState({inputThree})}
+                />
+                {/* <Input placeholder="Enter sentence for picture #3" /> */}
               </Item>
             </View>
             <View style={{padding: 70, justifyContent: 'center'}}>
-              <Button bordered dark>
+              <Button bordered dark onPress={this.handleMLU} style={{paddingRight: 40}}>
+                <Icon name="calculator" />
                 <Text>Calculate MLU</Text>
               </Button>
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Card style={{width: 70, height: 50}}>
+                <CardItem>
+                  <Body style={{alignItems: 'center'}}>
+                    <Text style={{textAlign: "center", fontSize: 20}}>
+                      {this.state.totalMLU}
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>   
             </View>
           </View>
         }
